17ca1a6e3e5ee19c1104d99f48851abbee32d231
Manuel Novoa <mnovoa@vmware.com>
2017-10-17 17:58:12 -0400

    update parser implementation

--- example.js
+++ example.js
@@ -1,15 +1,13 @@
 var parser = require('./index');
 
 // xml to json
-var xml = "<root version=\"1.2\"><inventory serial=\"1234342\">My Inventory</inventory><tet id=\"1\">234</tet><tet id=\"2\">23234</tet></root>";
+var xml = "<root version=\"1.2\"><inventory serial=\"1234342\">My Inventory</inventory><tet id=\"1\"><otro attraa=\"23423\"><ll>1</ll><la>134</la></otro></tet><tet id=\"2\">23234</tet></root>";
 console.log("input -> %s", xml)
 
-var optt = {
-    alternateTextNode: "myValue"
-}
-
-var json = parser.toJson(xml, optt);
+var json = parser.toJson(xml);
 console.log("to json -> %s", json);
 
-var xml = parser.toXml(json, optt);
-console.log("back to xml -> %s", xml)
+var xml1 = parser.toXml(json);
+console.log("back to xml -> %s", xml1)
+
+console.log(xml == xml1)
